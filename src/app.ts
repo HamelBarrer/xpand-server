7376582ee220ba67f2c4caa9d4aa73922379d436
Hamel Yesid Barrero Castaño <hamel_-barrero@hotmail.com>
2023-12-02 20:31:13 -0500

    add test endpoint

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/', (_, res) => {
+  res.status(200).json({ message: 'hello' });
+});
+
 app.use('/api/v1/notes', noteRoute);
 
 export default app;
